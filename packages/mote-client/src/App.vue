19ddcbc223625c139946c85eba5dc2f08d57e11c
Ben Truyman <ben@truyman.com>
2018-07-20 23:24:15 -0700

    feat(build): add basic front-end build system

---
+++ packages/mote-client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  background-color: #30383D;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  height: 1536px;
+  width: 2048px;
+}
+</style>
